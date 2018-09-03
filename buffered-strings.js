487cd7dda7fd0aad9bd4e50d3ec2d6a597a0a3a4
MPriest <miche.priest@gmail.com>
2018-09-03 14:43:02 -0700

    added ability to buffer the data

--- buffered-strings.js
+++ buffered-strings.js
@@ -8,7 +8,7 @@ function getAndPrintHTML () {
     };
   
     /* Add your code here */
-    
+    var buffer = ''
     // https.get takes a callback with one parameter -
     // response, which is a Stream that represents the HTTP response
     https.get(requestOptions, function (response) {
@@ -17,11 +17,13 @@ function getAndPrintHTML () {
         // the callback is invoked when a `data` chunk is received
         response.on('data', function (data) {
             console.log('Chunk Received. Length:', data.length);
+            buffer += data;
         });
         // the callback is invoked when all of the data has been received
         // (the `end` of the stream)
         response.on('end', function() {
             console.log('Response stream complete.');
+            console.log(buffer);
         });
     });
 
